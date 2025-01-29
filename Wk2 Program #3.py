6355d9f5a9e408f0ef8c5b648659baa009fc646f
reilly-kurth <rkkurth@students.unwsp.edu>
2025-01-29 16:27:42 -0600

    Add files via upload

---
+++ Wk2 Program #3.py
@@ -0,0 +1,12 @@
+item1 = float(input("Enter price of item 1: $"))
+item2 = float(input("Enter price of item 2: $"))
+item3 = float(input("Enter price of item 3: $"))
+item4 = float(input("Enter price of item 4: $"))
+item5 = float(input("Enter price of item 5: $"))
+
+subtotal = (item1 + item2 + item3 + item4 + item5)
+print("The subtotal of the purchases is ", subtotal)
+
+tax = 0.07
+sales_tax = subtotal * tax
+print("The sales tax of the purchases is ", sales_tax)
