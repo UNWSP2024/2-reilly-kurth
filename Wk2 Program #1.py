be4b44b3fb68b9a41eedaff1728e3611d4b32195
reilly-kurth <rkkurth@students.unwsp.edu>
2025-01-29 16:26:11 -0600

    Add files via upload

---
+++ Wk2 Program #1.py
@@ -0,0 +1,11 @@
+name= "Reilly Kurth"
+print("Your name:", name)
+
+address= "12345 67th St, Flower, 98765"
+print("Your address:" , address)
+
+phone_number = "111-222-3334"
+print("Your phone number:", phone_number)
+
+college_major = "Data Analytics"
+print("Your college major:", college_major)
